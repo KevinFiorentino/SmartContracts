a1d5e2a55d0df160e70396b209750259a90100db
Kevin <kevinfiorentino.1294@gmail.com>
2022-04-30 14:39:25 -0300

    SmartContract SendETH.sol

--- contracts/1. Variables.sol
+++ contracts/1. Variables.sol
@@ -2,21 +2,18 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-contract Identity {
+contract Variables {
     
     uint256 public idNumber;
     bool public isWorking;
     string public name;
     address public wallet;
 
-    constructor(
-        uint256 _idNumber,
-        bool _isWorking,
-        string memory _name
-    ) {
+    constructor(uint256 _idNumber, bool _isWorking, string memory _name) {
         idNumber = _idNumber;
         isWorking = _isWorking;
         name = _name;
         wallet = msg.sender;
     }
+    
 }
